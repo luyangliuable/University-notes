b1b8383b91f349b1592c596ff467765c216c7f3f
Luyang Liu <https://github.com/luyangliuable>
2022-03-13 18:42:21 +1100

    Added count sort algorithm

--- FIT2004/count_sort.py
+++ FIT2004/count_sort.py
@@ -1,20 +1,34 @@
-def count_sort(dictionary: list, input_list: list) -> tuple:
-    res  = []
+import random
+import time
+
+
+def count_sort(dictionary: list[int], input_list: list[int]) -> tuple:
 
     count = [0]*len(dictionary)
     pos = [0]*len(dictionary)
 
-    for i in range(len( input_list )):
-        count[input_list[i]] += 1
+    for i in input_list:
+        count[i] += 1
 
-    for i in range(len(count)):
+    for i in range(len(dictionary)):
         pos[i] = sum(count[:i])
+        # pos[i] = pos[i-1] + count[i-1]
 
-    return count, pos
+    temp = [0]*len(input_list)
+    for i in range(len(input_list)):
+        temp[pos[input_list[i]]] = input_list[i]
+        pos[input_list[i]] += 1
+
+    return count, pos, temp
 
 
 dictionary=[0,1,2,3,4,5,6,7,8,9,10]
+input = [ random.choice(dictionary) for _ in range(10) ]
 
-input = [1,2,3,2,3,3,2,1,3,2,3,2,2]
 
-print( count_sort(dictionary, input) )
+print("going to count sort", input)
+start = time.process_time()
+result = count_sort(dictionary, input)[2]
+end = time.process_time() - start
+print("result:",  result)
+print("result took", str(end) + "s", "to run")
---
+++ architecture/software/domain_modellin/readme.md
@@ -0,0 +1,23 @@
+# Domain Modelling
+2022-03-11
+<!-- markdown-toc start - Don't edit this section. Run M-x markdown-toc-refresh-toc -->
+**Table of Contents**
+
+- [Domain Modelling](#domain-modelling)
+    - [Definition](#definition)
+    - [Conceptual class](#conceptual-class)
+
+<!-- markdown-toc end -->
+
+## Definition
+
+> A domain model is a representation of a real-world conceptual classes, not of software component, it is not of diagrams describing software classes or software objects with responsibilities. <br />
+> Larman
+
+## Conceptual class
+* A conceptual class is an idea, thing or object.
+* We model identity, state and behaviour.
+* It represents things in the real world.
+
+## How to capture domain modelling?
+* UML Class diagram
